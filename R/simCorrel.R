1d1bad604f744a6c2eb6678f1e48fd5dac6908ef
qikushu <45323386+qikushu@users.noreply.github.com>
2022-04-08 19:40:04 +0900

    Update simCorrel.R

--- R/simCorrel.R
+++ R/simCorrel.R
@@ -1,1 +1,11 @@
-d = read.table("clipboard", head=T, row.names = "教科")
+d = read.table("https://raw.githubusercontent.com/qikushu/stat/master/R/correlMatrixBySubject.txt", comment.char="#", head=T, row.names = "教科", fileEncoding = "UTF-8-BOM")
+install.packages("mvtnorm")
+library(mvtnorm)
+
+#X1～X5の平均値
+m <- c(60, 60, 60, 60, 60)
+sigma = as.matrix(d)
+colnames(sigma) = c()
+rownames(sigma) = c()
+y <- rmvnorm(100, m, sigma*300, method = "chol")  
+
