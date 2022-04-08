3971d352b647b98a586a9f92e6e47e0e123aae1e
qikushu <45323386+qikushu@users.noreply.github.com>
2022-04-08 21:34:32 +0900

    Update simCorrel.R

--- R/simCorrel.R
+++ R/simCorrel.R
@@ -44,8 +44,14 @@ plot(scoreJapanese, rnorm(nrow(y5)), xlab = "Japanese", ylab="dummy", col=y5$rai
 # 数学と国語
 plot(y5$Math, y5$Japanese, xlab = "Math", ylab="Japanese", col=y5$rainbowColor, pch=20)
 # 数学と理科
-plot(y5$Math, y5$Science, xlab = "Math", ylab="JapScienceanese", col=y5$rainbowColor, pch=20)
+plot(y5$Math, y5$Science, xlab = "Math", ylab="Science", col=y5$rainbowColor, pch=20)
 
+# 三次元plot
+library(rgl)
+plot3d(y5, col=y5$rainbowColor)
+
+
+##############
 # 主成分分析
 rpca=prcomp(x=y4,scale=T)
 # 第1主成分
@@ -54,17 +60,9 @@ PC1 = rpca$x[,1]
 PC2 = rpca$x[,2]
 # 第3主成分
 PC3 = rpca$x[,3]
-
-# 主成分スコアの相関係数を見てみる
-# 第一主成分と第二主成分
-plot(PC1, PC2)
-cor(PC1, PC2)
-# 第二主成分と第三主成分
-plot(PC2, PC3)
-cor(PC2, PC3)
-# 第三主成分と第一主成分
-plot(PC3, PC1)
-cor(PC3, PC1)
+# 三次元プロット
+pcaRotated= as.data.frame(cbind(PC1, PC2, PC3,rainbowColor))
+plot3d(pcaRotated, col=pcaRotated$rainbowColor)
 
 # 第一主成分と各教科の得点の関係
 plot(PC1, scoreMath)
@@ -82,13 +80,19 @@ cor(PC2, scoreScience)
 plot(PC2, scoreJapanese)
 cor(PC2, scoreJapanese)
 
+# 第一主成分、第二主成分、第三主成分のあいだには相関があるか
+# 第一主成分と第二主成分
+plot(PC1, PC2)
+cor(PC1, PC2)
+# 第二主成分と第三主成分
+plot(PC2, PC3)
+cor(PC2, PC3)
+# 第三主成分と第一主成分
+plot(PC3, PC1)
+cor(PC3, PC1)
 
-# 三次元plot
-#library(rgl)
-#plot3d(y5, col=y5$rainbowColor)
 
-#pcaRotated= as.data.frame(cbind(PC1, PC2, PC3,rainbowColor))
-#plot3d(pcaRotated, col=pcaRotated$rainbowColor)
+
 
 
 
