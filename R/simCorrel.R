6ee865c39cb30640f52263b0b9ab54273554cc48
qikushu <45323386+qikushu@users.noreply.github.com>
2022-04-08 19:48:00 +0900

    Update simCorrel.R

--- R/simCorrel.R
+++ R/simCorrel.R
@@ -8,5 +8,12 @@ m <- c(60, 60, 60, 60, 60)
 sigma = as.matrix(d)
 colnames(sigma) = c()
 rownames(sigma) = c()
-y <- rmvnorm(100, m, sigma*300, method = "chol")  
+y <- rmvnorm(100, m, sigma*300, method = "chol") 
+
+# 100点を超えた場合は100点にする
+y[y > 100] = 100
+# マイナス点になった場合は0店にする
+y[y < 0] = 0
+
+
 
