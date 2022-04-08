15b4de075adc4adafd2ceb41e3103734d8fa3469
qikushu <45323386+qikushu@users.noreply.github.com>
2022-04-08 21:01:33 +0900

    Update simCorrel.R

--- R/simCorrel.R
+++ R/simCorrel.R
@@ -27,5 +27,38 @@ y4 = y3[order(y3$meanScore),]
 rainbowColor = heat.colors(nrow(y4))
 y5 = as.data.frame(cbind(y4,rainbowColor))
 
+#一次元データ
+# 数学の分布
+plot(y5$Math, rnorm(nrow(y5)), xlab = "Math", ylab="dummy", col=y5$rainbowColor, pch=20)
+# 理科の分布
+plot(y5$Science, rnorm(nrow(y5)), xlab = "Science", ylab="dummy", col=y5$rainbowColor, pch=20)
+# 国語の分布
+plot(y5$Japanese, rnorm(nrow(y5)), xlab = "Japanese", ylab="dummy", col=y5$rainbowColor, pch=20)
+
+# 二次元データ
+# 数学と国語
+plot(y5$Math, y5$Japanese, xlab = "Math", ylab="Japanese", col=y5$rainbowColor, pch=20)
+# 数学と理科
+plot(y5$Math, y5$Science, xlab = "Math", ylab="JapScienceanese", col=y5$rainbowColor, pch=20)
+
+# 三次元plot
 library(rgl)
 plot3d(y5, col=y5$rainbowColor))
+
+# 主成分分析
+rpca=prcomp(x=y4,scale=T)
+# 第1主成分
+PC1 = rpca$x[,1]
+# 第2主成分
+PC2 = rpca$x[,2]
+# 第3主成分
+PC3 = rpca$x[,3]
+
+pcaRotated= as.data.frame(cbind(PC1, PC2, PC3,rainbowColor))
+
+plot3d(pcaRotated, col=pcaRotated$rainbowColor))
+
+# 解釈させる
+
+
+
