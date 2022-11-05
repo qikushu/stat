026378ae756ef3737ddb3a28e15fd2b31c3874ff
Yoshiyuki Yamagata <yoshiyuk@agr.kyushu-u.ac.jp>
2022-11-05 14:00:59 +0900

    ok

--- genet/runSamplingLD.R
+++ genet/runSamplingLD.R
@@ -7,145 +7,28 @@ source("https://raw.githubusercontent.com/qikushu/stat/master/genet/samplingLD.R
 argnum=length(commandArgs())
 args<-commandArgs()[5:argnum]
 head1 = args[1]
+vcffile = args[2]
+chr = args[3]
 
 print(head1)
 # サンプリング開始
 count=10000
 maxdist = 10000000 #10 Mbp
-chr="chr01"
-VCFraw <- read.vcfR("myccnip2_chr01_cleaned_snps.vcf.gz")
+VCFraw <- read.vcfR(vcffile)
 glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
+
+rm(VCFraw)
+rm(glVCF)
+
+gc(reset = TRUE)
+gc(reset = TRUE)
+
 mat1 = as.matrix(glVCF)  # 行列に変換
 resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
+colnames(resultout) = c("distance", "r2", "Dprime")
 
 resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
 # output to file
 outfilename = paste0("LDlist",chr,"_", head1, ".txt")
 write.table(resultout, outfilename)
 
-chr="chr02"
-VCFraw <- read.vcfR("myccnip2_chr02_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-
-
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr03"
-VCFraw <- read.vcfR("myccnip2_chr03_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-
-
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr04"
-VCFraw <- read.vcfR("myccnip2_chr04_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr05"
-VCFraw <- read.vcfR("myccnip2_chr05_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr06"
-VCFraw <- read.vcfR("myccnip2_chr06_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr07"
-VCFraw <- read.vcfR("myccnip2_chr07_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr08"
-VCFraw <- read.vcfR("myccnip2_chr08_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr09"
-VCFraw <- read.vcfR("myccnip2_chr09_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr10"
-VCFraw <- read.vcfR("myccnip2_chr10_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr11"
-VCFraw <- read.vcfR("myccnip2_chr11_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
-
-chr="chr12"
-VCFraw <- read.vcfR("myccnip2_chr12_cleaned_snps.vcf.gz")
-glVCF <- vcfR2genlight(VCFraw)  # genlightオブジェクトの生成
-mat1 = as.matrix(glVCF)  # 行列に変換
-resultout = data.frame(matrix(rep(NA, 3), nrow=1))[numeric(0), ]
-colnames(resultout) = c("distance", "r", "Dprime")
-resultout = rbind(resultout, samplingLD(count=count, matSelected = mat1[,glVCF$chromosome == chr], maxdist= maxdist))
-# output to file
-outfilename = paste0("LDlist",chr,"_", head1, ".txt")
-write.table(resultout, outfilename)
